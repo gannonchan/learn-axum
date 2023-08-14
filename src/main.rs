c13fa16c53d8dab9e289de9866fa6da7e3f8367b
gannon <bim@outlook.com>
2023-08-14 23:28:19 +0800

    增加文件上传和模板引擎示例

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 use std::net::SocketAddr;
+use std::time::Duration;
 
 use axum::Extension;
 use sqlx::{MySql, Pool};
+use tower::ServiceBuilder;
+use tower_http::cors::{AllowHeaders, AllowMethods, AllowOrigin, CorsLayer};
+use tower_http::timeout::TimeoutLayer;
 
 mod routers;
 mod domain;
@@ -27,9 +31,26 @@ async fn main() -> anyhow::Result<()> {
     // let service_builder = ServiceBuilder::new();
     let pool = db::mysql_pool(db_uri).await?;
     let app_stat = AppDbStat { pool };
+    let mut tera = tera::Tera::new("static1/templates/**/*")?;
+    tera.full_reload()?;
+    // 处理跨域访问
+    let cors_layer = CorsLayer::new()
+        .allow_headers(AllowHeaders::any())
+        .allow_methods(AllowMethods::any())
+        .allow_credentials(false)
+        .allow_origin(AllowOrigin::any())
+        .max_age(Duration::from_secs(3600));
+    let middleware = ServiceBuilder::new()
+        // 处理超时请求
+        .layer(TimeoutLayer::new(Duration::from_secs(10)))
+        // 添加跨域处理层
+        .layer(cors_layer)
+        ;
     let routes = routers::routers()
         // 使用请求扩展进行外部传递
         .layer(Extension(app_stat))
+        .layer(Extension(tera))
+        .layer(middleware)
         // 使用状态提取器进行外部传递
         // .with_state(app_stat)
         ;
--- src/routers/mod.rs
+++ src/routers/mod.rs
@@ -1,6 +1,10 @@
+use axum::{Extension, Router};
 use axum::body::Body;
-use axum::Router;
-use axum::routing::get;
+use axum::extract::Multipart;
+use axum::http::StatusCode;
+use axum::response::Html;
+use axum::routing::{get, post};
+use tera::{Context, Tera};
 
 use payment_router::payment_router;
 use user_router::user_router;
@@ -19,6 +23,8 @@ pub fn routers() -> Router<(), Body> {
     Router::new()
         // `GET /` goes to `root`
         .route("/", get(root))
+        .route("/tera", get(tera))
+        .route("/upload", post(upload))
         .merge(payment_router())
         .merge(user_router())
         .merge(sys_user_router())
@@ -29,4 +35,31 @@ pub fn routers() -> Router<(), Body> {
 // basic handler that responds with a static string
 pub async fn root() -> &'static str {
     "Hello, World!"
+}
+
+pub async fn tera(Extension(tera): Extension<Tera>) -> Result<Html<String>, String> {
+    let mut context = Context::new();
+    context.insert("message", "testTera");
+    let page = tera.render("index.html", &context)
+        .map_err(|err| err.to_string())?;
+    Ok(Html(page))
+}
+
+/// 上传操作
+async fn upload(mut multipart: Multipart) -> Result<(StatusCode, String), String> {
+    if let Some(file) = multipart.next_field().await.unwrap() {
+        let filename = file.file_name().unwrap().to_string(); // 上传的文件名
+        let data = file.bytes().await.unwrap(); // 上传的文件的内容
+
+        // 保存上传的文件
+        tokio::fs::write(&filename, &data)
+            .await
+            .map_err(|err| err.to_string())?;
+        return Ok((StatusCode::CREATED, format!(
+            "【上传的文件】文件名：{:?}, 文件大小：{}",
+            filename,
+            data.len()
+        )));
+    }
+    Ok((StatusCode::NO_CONTENT, String::from("没有上传文件")))
 }
